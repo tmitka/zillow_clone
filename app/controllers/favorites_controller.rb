d56cdc6bd6a9d8e1f7b27f5c90f03ae699251026
CristyMona11 <cristina.monarrez.11@gmail.com>
2017-06-21 18:24:46 -0500

    tiny willow

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,31 +2,23 @@ class FavoritesController < ApplicationController
 
 def new
 @fav = Favorite.new(user:User.find(session[:current_user_id]), property:Property.find(params[:id]))
-if @fav.save
-  redirect_to '/properties'
-  flash[:success] = "Favorited!"
-else
-  redirect_to '/properties'
-end
+  if @fav.save
+    redirect_to '/properties'
+    flash[:success] = "Favorited!"
+  else
+    redirect_to '/properties'
+  end
 end
 
 def index
   @properties = []
   @favorites = Favorite.where(user:session[:current_user_id])
   @favorites.each do |fav|
+    puts fav.property
     @properties.append(Property.find(fav.property))
-  end
+end
 
 def destroy
   @fav = Favorite.where(user:User.find(session[:current_user_id]), property:Property.find(params[:property_id]))
-  puts @fav
   redirect_to '/favorites'
-
-end
-
-
-end
-
-
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# users = User.create(first_name:"Mark", last_name:"Lehn", email:"marklehn@icloud.com", password_digest:"asdfasdf", phone_number:"6511231233", agent:true)
+#
+# properties = Property.create(
+# [{ address: "123 6th St. Melbourne, FL", bedrooms: 2, bathrooms: 3, zipcode: 32904, user: 1, sq_ft: 300, rent: true},
+# { address: "71 Pilgrim Avenue
+# Chevy Chase, MD", bedrooms: 2, bathrooms: 1, zipcode: 20815, user: 1, sq_ft: 400, rent: true}])
